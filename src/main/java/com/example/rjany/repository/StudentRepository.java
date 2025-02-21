28c9a128e6175f6058ebafccd82bf1b0477398f2
rrahhal1 <37365408+rrahhal1@users.noreply.github.com>
2025-02-21 13:17:23 +0200

    Delete src directory

--- src/main/java/com/example/rjany/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rjany.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.rjany.model.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
-}
